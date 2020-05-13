bcdfbd77d63154273f5c7837c8586cffbb310590
Timo Müller <muellertimomueller@gmail.com>
2020-05-13 14:36:19 +0200

    refactored switching the theme and added better documentation

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
 
 
 $(document).ready(() => {
+    console.log("Website is ready.")
+
     var theme = "light"
 
-    $("#switchTheme").click(() => {
-        if(theme == "light"){
-            theme = "dark"
-            $('#bulma_theme').attr('href','https://jenil.github.io/bulmaswatch/darkly/bulmaswatch.min.css');
-        }else{
-            theme = "light"
-            $('#bulma_theme').attr('href','https://jenil.github.io/bulmaswatch/flatly/bulmaswatch.min.css');
-        }
-    })
+    // Switch the theme on the click of the button
+    // (Only visible to users without sensor)
+    $("#switchTheme").click(switchTheme)
 
+    // Check if the sensor is accessible
+    if (window.AmbientLightSensor) {
+        try {
+            // Try to create a new sensor through wich the values can be accessed
+            const sensor = new AmbientLightSensor();
+            // Callback is executed as soon as the sensor returns a new value
+            sensor.onreading = () => {
+                console.log(sensor.illuminance)
+                // sensor.illumninance represents light level in lux
+                // according to official doc: < 50 <=> "dark"
+                if (sensor.illuminance < 50) {
+                    // TODO: switch to dark mode theme
+                    console.log("dark light")
+                } else {
+                    //TODO: switch to light mode theme
+                    console.log("bright light")
+                }
+            }
 
-
-    console.log("Website has loaded - adding Event Listener for Ambient Light")
-    if (window.AmbientLightSensor){
-        try{
-          const sensor = new AmbientLightSensor();
-          // Detect changes in the light
-          sensor.onreading = () => {
-            console.log(sensor.illuminance)
-    
-              // Read the light levels in lux 
-              // < 50 is dark room
-              if (sensor.illuminance < 50) {
-                // TODO: switch to dark mode theme
-
-                console.log("dark light")
-              } else {
-                //TODO: switch to light mode theme
-                console.log("bright light")
-              }
-          }
-    
-          // Has an error occured?
-          sensor.onerror = event => console.error(event.error.message)
-          sensor.start();
-        } catch(err) {
-          console.error(err.message)
+            // Error handling of the sensor itself
+            sensor.onerror = event => console.error(event.error.message)
+            sensor.start();
+        } catch (err) {
+            // Error handling of everything else above
+            console.error(err.message)
         }
     } else {
-      console.log("Browser doesn't support ambient light")
-      $("#error").show()
+        // The browser does not support ambient lighting, display that to the user
+        $("#error").show()
+    }
+
+
+
+
+    function switchTheme() {
+        if (theme == "light") {
+            theme = "dark"
+            $('#bulma_theme').attr('href', 'https://jenil.github.io/bulmaswatch/darkly/bulmaswatch.min.css');
+        } else {
+            theme = "light"
+            $('#bulma_theme').attr('href', 'https://jenil.github.io/bulmaswatch/flatly/bulmaswatch.min.css');
+        }
     }
 })
