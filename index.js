9c3c9596263701825bf6696bf93985493af8ae5e
Timo Müller <muellertimomueller@gmail.com>
2020-05-13 14:27:12 +0200

    added logic to switch theme

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+
+
 $(document).ready(() => {
+    var theme = "light"
+
+    $("#switchTheme").click(() => {
+        if(theme == "light"){
+            theme = "dark"
+            $('#bulma_theme').attr('href','https://jenil.github.io/bulmaswatch/darkly/bulmaswatch.min.css');
+        }else{
+            theme = "light"
+            $('#bulma_theme').attr('href','https://jenil.github.io/bulmaswatch/flatly/bulmaswatch.min.css');
+        }
+
+        console.log("switchting theme to " + theme)
+
+        
+
+
+    })
+
+
+
     console.log("Website has loaded - adding Event Listener for Ambient Light")
     if (window.AmbientLightSensor){
         try{
@@ -11,7 +33,7 @@ $(document).ready(() => {
               // < 50 is dark room
               if (sensor.illuminance < 50) {
                 // TODO: switch to dark mode theme
-                
+
                 console.log("dark light")
               } else {
                 //TODO: switch to light mode theme
